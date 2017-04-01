d5fd3f4770757f5144c943631ef0fc8e7d2007c8
Stoian Catalin <shaggy.cata@gmail.com>
2017-04-01 22:58:15 +0300

    remove unused file

--- routes/all.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var path = require('path');
-
-router.get('*', function(req, res, next) {
-    res.sendFile(path.join(__dirname + '/../public/dist/index.html'));
-});
-
-module.exports = router;
